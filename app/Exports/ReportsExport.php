75e59406e5119289b19c67459cf0dff56e2b3412
Ekky Novriza Alam <ekkynovriza@gmail.com>
2021-06-22 09:40:44 +0700

    update export file

--- app/Exports/ReportsExport.php
+++ app/Exports/ReportsExport.php
@@ -5,11 +5,14 @@ namespace App\Exports;
 use App\Models\report;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class ReportExport implements FromCollection
+class ReportsExport implements FromCollection
 {
-    public function DetailCollection()
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
     {
-	    $report = new report();
+        $report = new report();
         return $report->resumeByClass('SI4202','2021-02-01','2021-07-01');
     }
 }
